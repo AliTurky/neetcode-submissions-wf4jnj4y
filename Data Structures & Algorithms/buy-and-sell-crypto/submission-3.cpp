3b19e77313683e36a4dd7f49e7fb98963303e742
AliTurky <128913029+AliTurky@users.noreply.github.com>
2026-04-29 07:51:39 +0300

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int maxProfit(vector<int>& prices) {
+    int l=0;
+    int r = l+1;
+    int maxProfit=0;
+    while(r<prices.size())
+    {
+
+        if(prices[l]<prices[r])
+        {
+            maxProfit = max(maxProfit,(prices[r]-prices[l]));
+
+        }
+        else
+        {
+            l=r;
+        }
+        r++;
+    }
+    return maxProfit;
+}
+};
