ac17ebdc1c16aea419c608d510829538cc6be02e
AliTurky <128913029+AliTurky@users.noreply.github.com>
2026-08-23 22:14:04 +0300

    Add: sqrtx - submission-9

---
+++ Data Structures & Algorithms/sqrtx/submission-9.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int mySqrt(int x) {
+    int left=0;int right=x;
+    int res=0;
+    while(left<=right)
+    {
+        int mid = left+(right - left)/2;
+        if((long long)mid*mid == x )
+        {
+            return mid;
+        }
+        else if((long long)mid*mid > x)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+             res = mid;
+        }
+
+    }
+    return res;
+}
+};
